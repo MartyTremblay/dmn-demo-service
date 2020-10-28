bfda38716f6e3455be8e80a3c18aa63b34c938a9
bluepitch <ravi.mangalagiri@bluepitch.com>
2020-10-28 11:26:38 -0400

    added smallrye ui

--- src/main/java/com/thirtySevenDecisions/demo/KogitoApplication.java
+++ src/main/java/com/thirtySevenDecisions/demo/KogitoApplication.java
@@ -2,26 +2,12 @@ package com.thirtySevenDecisions.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication(scanBasePackages = { "com.thirtySevenDecisions.demo.**", "org.kie.kogito.**" })
 public class KogitoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KogitoApplication.class, args);
-    }
-
-    @Bean
-    public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
     }
 
 }
